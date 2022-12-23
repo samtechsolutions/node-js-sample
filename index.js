07437cd31dc0335fc623e4b0fb932a9e29a18081
ejazali1510 <91586657+ejazali1510@users.noreply.github.com>
2022-12-23 10:54:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World this is EjAZ! Ali MD')
+  response.send('Hello World this is EjAZ! Ali MD.....')
 })
 
 app.listen(app.get('port'), function() {
