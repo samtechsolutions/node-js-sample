87094d2066918ae6bc12de0b7339f2e2fb87c0b1
AMARAVATHIP <113093201+AMARAVATHIP@users.noreply.github.com>
2022-12-26 15:11:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World this is Dinesh kommisetty here..')
+  response.send('Hello World this is amaravathi here..')
 })
 
 app.listen(app.get('port'), function() {
